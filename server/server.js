488fdf2f2ff5210343a1b68daa9d75529ac1665c
lynetterussel331 <lynette.chua3195@gmail.com>
2020-07-26 12:02:45 +0800

    Fix config for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4200;
 const app_path = '../dist/webui';
 
 app.use('/', express.static(path.join(__dirname, app_path)))
